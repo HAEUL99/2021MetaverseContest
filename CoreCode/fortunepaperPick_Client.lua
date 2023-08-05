1e30afbe627e9b826fec426f2a57b62737910e86
HAEUL99 <71214577+HAEUL99@users.noreply.github.com>
2023-08-05 19:23:12 +0900

    Update fortunepaperPick_Client.lua

--- CoreCode/fortunepaperPick_Client.lua
+++ CoreCode/fortunepaperPick_Client.lua
@@ -18,18 +18,21 @@ local function EnablePressUI(self, target)
     end
 
     if NoticeCheck == false and Check == false then
+        Input:AddGroup("ShopNoticeInput") 
+        -- ShopNoticeInput 인풋 이벤트 그룹에, ShopNotice 이벤트(f키 입력) 연결
+        Input:AddActionKeyEvent("ShopNoticeInput", "ShopNotice", Enum.Key.F)
+        
+        NoticeCheck = true
+        -- ShopNoticeInput 활성화
+        Input:ActiveGroup("ShopNoticeInput") 
+        NoticeUI:SetVisible(true)
+        
         Input:AddGroup("ShopSetInput")
         Input:AddActionKeyEvent("ShopSetInput", "EndShopSet", Enum.Key.SpaceBar)
-
-        Input:AddGroup("ShopNoticeInput")
-        Input:AddActionKeyEvent("ShopNoticeInput", "ShopNotice", Enum.Key.F)
-   
-        NoticeCheck = true
-        Input:ActiveGroup("ShopNoticeInput")
-        NoticeUI:SetVisible(true)
     end
 end
-CheckCollider.OnBeginOverlapEvent:Connect(EnablePressUI)
+-- 지정장소의 collider에 입장하면, EnablePressUI함수 실행.
+CheckCollider.OnBeginOverlapEvent:Connect(EnablePressUI) 
 
 
 local function DisablePressUI(self, target)
@@ -39,6 +42,7 @@ local function DisablePressUI(self, target)
 
     if NoticeCheck then
         NoticeCheck = false
+        -- 인풋 이벤트 제거
         Input:RemoveGroup("ShopNoticeInput")
         Input:RemoveGroup("ShopSetInput")
         NoticeUI:SetVisible(false)
@@ -46,12 +50,14 @@ local function DisablePressUI(self, target)
 end
 CheckCollider.OnEndOverlapEvent:Connect(DisablePressUI)
 
+-- ShopNotice이벤트 발생시,
 LocalPlayer:ProcessInputActionEvent("ShopNotice", Enum.KeyInputType.Pressed, function()
    if NoticeCheck == false then
        return
    end
-   shop_s:SendEventToServer("StartShopSet")
+   shop_s:SendEventToServer("StartShopSet") -- shop_s객체의 StartShopSet 서버이벤트를 호출함.
 end)
+
 
 LocalPlayer:ProcessInputActionEvent("EndShopSet", Enum.KeyInputType.Pressed, function()
    if Check == false then
@@ -62,12 +68,12 @@ end)
 
 
 local function StartSetShop()
-   Input:ActiveGroup("ShopSetInput")
+   Input:ActiveGroup("ShopSetInput") -- 인풋 이벤트 ShopSetInput 활성화
 
    NoticeUI:SetVisible(false)
    
    local character = LocalPlayer:GetRemotePlayer():GetCharacter()
-   character:AttachAtTop(shop_s, Enum.Bone.LFoot)
+   character:AttachAtTop(shop_s, Enum.Bone.LFoot) --플레이어를 지정위치에 고정시킴.
    wait(0.1)
 
 end
@@ -76,7 +82,7 @@ shop_s:ConnectEventFunction("StartSetShopClient", StartSetShop)
 local function EndShopSet()
    local character = LocalPlayer:GetRemotePlayer():GetCharacter()
    wait(1)
-   Game:SendEventToServer("SetItem", 0)
+   Game:SendEventToServer("SetItem", 0) 
    NoticeUI:SetVisible(true)
 end
 shop_s:ConnectEventFunction("EndShopSetClient", EndShopSet)
