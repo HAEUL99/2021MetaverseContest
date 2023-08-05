481e4a176c672ca8cd6d86548f0b867850443e5f
HAEUL99 <71214577+HAEUL99@users.noreply.github.com>
2023-08-05 19:35:42 +0900

    Create Selfi_Server.lua

---
+++ CoreCode/Selfi_Server.lua
@@ -0,0 +1,36 @@
+local cube = Script.Parent
+local area_list = {}
+
+
+cube:SetCharacterCollisionResponse(Enum.CollisionResponse.Overlap)
+local function BeginOverlapEvent(self, target) 
+    if target ~= nil and target:IsCharacter() then
+        table.insert(area_list, target:GetName())
+        Game:BroadcastEvent("Reset")
+        for i = 1, #area_list, 1 do
+            Game:BroadcastEvent("Camera_Mult", area_list[i])
+        end  
+        
+    end
+end
+
+cube.OnBeginOverlapEvent:Connect(BeginOverlapEvent) 
+
+local function EndOverlapEvent(self, target) 
+    if target ~= nil and target:IsCharacter() then
+        for key, value in pairs(area_list) do
+            if value == target:GetName() then
+                 table.remove(area_list, key)
+            end
+        end
+        Game:BroadcastEvent("Reset"
+        for i = 1, #area_list, 1 do
+            Game:BroadcastEvent("Camera_Mult", area_list[i]) 
+        end  
+        
+    end
+end
+cube.OnEndOverlapEvent:Connect(EndOverlapEvent) 
+
+
+
