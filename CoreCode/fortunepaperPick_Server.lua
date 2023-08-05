dcdb2c631aec954a22c90cf2af6a65672fc46e68
HAEUL99 <71214577+HAEUL99@users.noreply.github.com>
2023-08-05 19:23:32 +0900

    Update fortunepaperPick_Server.lua

--- CoreCode/fortunepaperPick_Server.lua
+++ CoreCode/fortunepaperPick_Server.lua
@@ -8,14 +8,20 @@ local function StartShopSet(player)
     end
     
     Owner = player:GetCharacter()
-    shop_s:SendEventToClient(player:GetPlayerName(), "StartSetShopClient")
+    -- 클라이언트 이벤트 StartSetShopClient를 호출함.
+    shop_s:SendEventToClient(player:GetPlayerName(), "StartSetShopClient") 
+    -- 클라이언트 이벤트 IsCharacterShopSet를 호출함. (매개변수 true)
     shop_s:BroadcastEvent("IsCharacterShopSet", true)
+    -- 운세통 attach
     Game:SendEventToClient(player:GetPlayerName(), "itemattach", 2)
+    -- ProgressUI 띄움.
     Game:SendEventToClient(player:GetPlayerName(),"IsPressedforShop")
     wait(10)
+    -- 미션 완료 UI 뛰움.
     Game:SendEventToClient(player:GetPlayerName(), "FillStart", 2)
     
 end
+-- shop_s객체의 서버 이벤트 StartShopSet에 StartShopSet()함수 연결
 shop_s:ConnectEventFunction("StartShopSet", StartShopSet)
 
 
@@ -26,6 +32,7 @@ local function EndShopSet(player)
    player:GetCharacter():Detach()
    shop_s:SendEventToClient(player:GetPlayerName(), "EndShopSetClient")
    shop_s:BroadcastEvent("IsCharacterShopSet", false)
+   --item 제거
    Game:SendEventToClient(player:GetPlayerName(), "itemattach", 2)
    wait(1)
    
